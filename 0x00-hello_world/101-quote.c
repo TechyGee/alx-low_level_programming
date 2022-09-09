2bcb10622d36cea99c144ba10477a0ce59066901
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-09-09 04:08:40 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora korpar,
@@ -9,8 +9,8 @@
 int main(void)
 {
 	write(2,
-			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-			59);
+		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+		59);
 
 	return (1);
 }
