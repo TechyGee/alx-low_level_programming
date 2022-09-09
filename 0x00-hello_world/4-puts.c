c87347f660a400fdb6e1c138ad515a00ab97f196
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-09-08 21:49:53 -0700

    error fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
- * Return: Always 0.
+ * main - Print "Programming is like building a multilingual puzzle, follow by a new line.
  *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle.\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
