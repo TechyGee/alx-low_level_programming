b6fd030ce49914d803eab6300f1086e15f43d610
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-09-09 04:01:07 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora korpar,
+ * 2015-10-19, follow by a new line, to the standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
