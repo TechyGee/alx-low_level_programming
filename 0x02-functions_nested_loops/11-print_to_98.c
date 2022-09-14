6e4df98aadc79b5349d1e86a7d239676f89689a2
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-09-14 08:43:30 -0700

    error fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,15 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			_putchar("%d, ", n--);
-		_putchar("%d\n", n);
+			printf("%d, ", n--);
+		printf("%d\n", n);
 
 	}
 
 	else
 	{
 		while (n < 98)
-			_putchar("%d, ", n++);
-		_putchar("%\n", n);
+			printf("%d, ", n++);
+		printf("%\n", n);
 	}
 }
