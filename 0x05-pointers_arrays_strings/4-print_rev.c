1deace59e185435ed0b0ec2e98414b355936dbd3
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-09-19 08:40:52 -0700

    prints string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,12 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0, index = 0;
 
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= 0; index++)
+	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
