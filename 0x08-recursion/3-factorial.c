45da478a3901ed2ada3472d57e7ebbc8feeae7ab
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-09-28 07:52:51 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,20 +10,14 @@
 
 int factorial(int n)
 {
+	int result = n;
+
 	if (n < 0)
 		return (-1);
 
 	else if (n >= 0 && n <= 1)
 		return (1);
-
-	else
-		return n * factorial(n - 1);
+	result = result * factorial(n-1);
 	
-}
-
-int main(void)
-{
-	int n = result;
-	_putchar("%d", result);
-	return (0);
+	return (result);	
 }
