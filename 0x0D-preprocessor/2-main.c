3b2e0dfc09d154e8cec4c958fcec52101d24464b
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-10-10 12:38:47 -0700

    file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,12 +5,12 @@
  * main - write a program that prints the name of the file
  * it was compiles fro, followed by a new line.
  *
- * Retrun: Always 0;
+ * Return: Always 0;
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return 0;
+	return (0);
 }
