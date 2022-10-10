6b7e1545f4ad76588e63ae11dbbd9a312a3b79d2
TechyGeeCoder <idrismukhtar2244@gmail.com>
2022-10-10 04:43:24 -0700

    pi header

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * File: Pi.h
+ * Auth: TechyGeeCoder
+ * Desck: Header File that defines a macro named
+ * PI as an abbreviation for the token 3.14159265359.
+ *
+ */
+
+#define PI 3,14159265359
+
+#endif
